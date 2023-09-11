ec6c62d416860920fb14b98771f782521648f803
eapatterson <122939310+eapatterson@users.noreply.github.com>
2023-09-11 15:05:37 -0400

    Update lab2.sql

--- lab2.sql
+++ lab2.sql
@@ -1,10 +1,10 @@
 -- Create schemas
 
 -- Create tables
-CREATE TABLE IF NOT EXISTS ""              professor""
+CREATE TABLE IF NOT EXISTS professor
 (
     p_email VARCHAR(50) NOT NULL,
-    p_name VARCHAR(50) NOT NULL,
+    p_name VARCHAR(100) NOT NULL,
     office VARCHAR(10) NOT NULL UNIQUE,
     PRIMARY KEY(p_email)
 );
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS ""              professor""
 CREATE TABLE IF NOT EXISTS student
 (
     s_email VARCHAR(50) NOT NULL,
-    s_name VARCHAR(50) NOT NULL,
+    s_name VARCHAR(100) NOT NULL,
     major VARCHAR(10) NOT NULL,
     PRIMARY KEY(s_email)
 );
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS student
 CREATE TABLE IF NOT EXISTS course
 (
     c_number VARCHAR(50) NOT NULL,
-    c_name VARCHAR(50) NOT NULL,
+    c_name VARCHAR(100) NOT NULL,
     room VARCHAR(10) NOT NULL,
     p_email VARCHAR(50) NOT NULL,
     PRIMARY KEY(c_number)
@@ -28,16 +28,16 @@ CREATE TABLE IF NOT EXISTS course
 
 CREATE TABLE IF NOT EXISTS enroll
 (
-    c_number VARCHAR(50) NOT NULL,
     s_email VARCHAR(50) NOT NULL,
-    PRIMARY KEY(c_number, s_email)
+    c_number VARCHAR(50) NOT NULL,
+    PRIMARY KEY(s_email, c_number)
 );
 
 
 -- Create FKs
 ALTER TABLE course
     ADD    FOREIGN KEY (p_email)
-    REFERENCES "              professor"(p_email)
+    REFERENCES professor(p_email)
     MATCH SIMPLE
 ;
     
@@ -55,4 +55,3 @@ ALTER TABLE enroll
     
 
 -- Create Indexes
-
