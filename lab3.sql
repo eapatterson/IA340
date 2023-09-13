481772b89ee0c736b5121135f7e36a35a115b244
eapatterson <122939310+eapatterson@users.noreply.github.com>
2023-09-13 16:21:56 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -3,20 +3,20 @@ insert into student(s_email,s_name,major)
 values('s1@jmu.edu','s1','IA'),
 	('s2@jmu.edu','s2','IA'),
 	('s3@jmu.edu','s3','ISAT'),
-	('s4@jmu.edu','s4','ISAT');
+	('s4@jmu.edu','s4','ISAT')
 	
 
 --Q2.2
 insert into professor(p_email,p_name,office)
 values('p1@jmu.edu','p1','o1'),
-  ('p2@jmu.edu','p2','o2');
+  ('p2@jmu.edu','p2','o2')
 
 --Q2.3
 insert into course(c_number,c_name,room,p_email)
 values('c1','postgresql','r1','p1@jmu.edu'),
   ('c2','mongodb','r2','p2@jmu.edu'),
-  ('c3','twitter','r1','p1@jmu.edu');
-
+  ('c3','twitter','r1','p1@jmu.edu')
+	
 --Q2.4
 insert into enroll(s_email,c_number)
 values('s1@jmu.edu','c1'),
@@ -24,6 +24,23 @@ values('s1@jmu.edu','c1'),
   ('s3@jmu.edu','c1'),
   ('s4@jmu.edu','c2'),
   ('s2@jmu.edu','c3'),
-  ('s3@jmu.edu','c3');
+  ('s3@jmu.edu','c3')
 
---Q2.5
+--Q2.5 
+/* you need to modify the professor table first because you need to add the new professor record before you can add it to the course.*/
+insert into professor(p_email,p_name,office)
+values('p3@jmu.edu','p3','o3');
+
+insert into course(c_number,c_name,room,p_email)
+values('c4','facebook','r1','p3@jmu.edu')
+
+--Q2.6 
+/*You need to modify the course table first because you need to change the p_email in the course before you delete the professors(p1) records. If p1 is teaching the course you cannot delete it*/
+update course
+set p_email = 'p3@jmu.edu'
+where p_email = 'p1@jmu.edu';
+
+delete from professor 
+where p_email = 'p1@jmu.edu'
+
+--Q2.7
